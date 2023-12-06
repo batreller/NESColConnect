187d5b4eba2aa6c1ddb72f16bbeaf7826243a76f
batreller <batreller@gmail.com>
2023-12-06 12:06:44 +0000

    Set up web server

---
+++ src/main/java/nescol/connect/NESColConnectApplication.java
@@ -0,0 +1,11 @@
+package nescol.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NESColConnectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NESColConnectApplication.class, args);
+    }
+}
