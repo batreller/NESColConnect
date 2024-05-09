6704184e538823e2388a9216801236da3744e60f
batreller <batreller@gmail.com>
2024-05-09 14:27:21 +0100

    add docker-compose.yml

--- src/main/resources/script.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE nescol_connect;
-CREATE USER 'nescol_connect_user'@'localhost' IDENTIFIED BY 'ncon';
-GRANT ALL PRIVILEGES ON nescol_connect.* TO 'nescol_connect_user'@'localhost';
-FLUSH PRIVILEGES;
-ALTER USER 'nescol_connect_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ncon';
